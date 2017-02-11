5855c615d026bbe4d5c346b0702a09432a2b11ea
gkaguirre <gkaguirre@me.com>
2017-02-11 09:54:57 -0500

    Configured for tbUseProject
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# surveyMelanopsinAnalysis
+
+Analysis code for surveys and demographic data collected as part of the MELA series of experiments.
---
+++ configuration/surveyMelanopsinAnalysisLocalHookTemplate.m
@@ -0,0 +1,26 @@
+function surveyMelanopsinAnalysisLocalHook
+% surveyMelanopsinAnalysisLocalHook
+%
+% Configure things for processing MELA project surveys.
+%
+% For use with the ToolboxToolbox.  Copy this into your
+% ToolboxToolbox localToolboxHooks directory (by defalut,
+% ~/localToolboxHooks) and delete "Template" from the filename
+%
+% The thing that this does is add subfolders of the project to the path as
+% well as define Matlab preferences that specify input and output
+% directories.
+%
+% You will need to edit the project location and i/o directory locations
+% to match what is true on your computer.
+
+%% Say hello
+fprintf('Running surveyMelanopsinAnalysisLocalHook\n');
+
+%% Set preferences
+
+% Point at the code
+projectDir = tbLocateProject('surveyMelanopsinAnalysis');
+
+% Add the code to the path
+addpath(genpath(projectDir));
