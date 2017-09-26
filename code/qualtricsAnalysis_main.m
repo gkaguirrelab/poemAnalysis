4e3cc12faf617c24681d48cc7bc65e5c26909ee2
gkaguirre <gkaguirre@me.com>
2017-09-26 14:21:27 -0400

    Added some brutal hackery to create a confusion matrix
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/qualtricsAnalysis_main.m
+++ code/qualtricsAnalysis_main.m
@@ -17,7 +17,6 @@ dropboxDir = ...
     fullfile('/Users', userName, '/Dropbox (Aguirre-Brainard Lab)');
 
 %% Set paths to data and output
-%qualtricsDataDir = '/MELA_subject/QualtricsData/';
 qualtricsDataDir = '/MELA_analysis/surveyMelanopsinAnalysis/Qualtrics/';
 analysisDir = '/MELA_analysis/surveyMelanopsinAnalysis/Qualtrics/';
 
@@ -25,14 +24,69 @@ analysisDir = '/MELA_analysis/surveyMelanopsinAnalysis/Qualtrics/';
 outputResultExcelName=fullfile(dropboxDir, analysisDir, 'MELA_QualtricsHeadacheResults.xlsx');
 rawDataSheets={'POEM_v1.0_9-23-2017_PPIremoved.csv'};
 
-% Loop through the datasheets
-
 % get the full path to thisDataSheet
 thisDataSheetFileName=fullfile(dropboxDir, qualtricsDataDir, rawDataSheets{1});
 
-% load and pre-process thisDataSheet, returning table "T"
+%% load and pre-process thisDataSheet, returning table "T"
 [T, notesText] = qualtricsAnalysis_preProcess(thisDataSheetFileName);
 
-% classify headache
+%% classify headache based upon table T
 diagnosisTable = qualtricsAnalysis_classifyHeadache( T );
 writetable(diagnosisTable,outputResultExcelName,'Range','A4','WriteRowNames',true)
+
+%% create a confusion matrix
+
+% load the study neurologist diagnoses
+goldStandardFile = 'ACHE-M_studyID+diagnosis.csv';
+goldStandardFileFullPath = fullfile(dropboxDir, analysisDir, goldStandardFile);
+goldStandardDiagnosis = readtable(goldStandardFileFullPath);
+
+% This is the order of columns in the confusion matrix. Note that we are
+% not including the Migraine with other aura here.
+reOrderedPoemDiagnosisLabels = {'HeadacheFree','MildNonMigrainousHeadache','HeadacheNOS','MigraineWithoutAura','MigraineWithVisualAura'};
+
+confusionMatrix = zeros(3,5);
+% loop through the subjects in the diagnosisTable
+for ss = 1:size(diagnosisTable,1)
+    % get this subject's studyID
+    studyID = diagnosisTable.Row{ss};
+    % see if there is an entry in the gold standard table
+    matchIdx = find(cellfun(@(x) strcmp(studyID,x), goldStandardDiagnosis.SubjectID));
+    % assign a confuion matrix row based upon the gold standard diagnosis
+    if ~isempty(matchIdx)
+        switch goldStandardDiagnosis.Diagnosis{matchIdx}
+            case ''
+                rowIdx = [];
+            case 'Control'
+                rowIdx = 1;
+            case 'Migraine without aura'
+                rowIdx = 2;
+            case 'Migraine with aura'
+                rowIdx = 3;
+            otherwise
+                error('I encountered an undefined gold standard diagnosis');
+        end
+        % Identify which poem diagnosis was assigned to this subject
+        initialColumnIdx = find(table2array(diagnosisTable(ss,1:6)),1);
+        % Find which column this diagnosis will be placed within
+        columnIdx = find( strcmp(reOrderedPoemDiagnosisLabels, diagnosisTable.Properties.VariableNames{initialColumnIdx}) ); 
+        if ~isempty(rowIdx)
+            confusionMatrix(rowIdx,columnIdx) = confusionMatrix(rowIdx,columnIdx) + 1;
+        end
+    end
+end
+
+% print out the confusion matrix
+outline='\t';
+for dd = 1:5
+    outline = [outline '\t' reOrderedPoemDiagnosisLabels{dd} '\t'];
+end
+fprintf([outline '\n']);
+goldStandardDiagnoses = {'Control\t\t','Migraine without aura','Migraine with aura'};
+for rr = 1:3
+    outline = [goldStandardDiagnoses{rr} '\t'];
+    for cc = 1:5
+        outline = [outline num2str(confusionMatrix(rr,cc)) '\t\t'];
+    end
+fprintf([outline '\n']);
+end
