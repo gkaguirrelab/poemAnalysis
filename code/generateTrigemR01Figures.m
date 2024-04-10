abd1c227823a92d4a8d36f7a60f4013d04ac9387
gkaguirre <gkaguirre@me.com>
2024-04-10 12:16:09 -0400

    Figures for R01 proposal

--- code/generateTrigemR01Figures.m
+++ code/generateTrigemR01Figures.m
@@ -10,30 +10,115 @@ nBoots = 100;
 nBins = 7;
 
 migraineDx = {'migraine w/out aura','migraine w/aura'};
+probMigraine = {'probable migraine w/out aura','probable migraine w/aura'};
 haFDx = {'no headache'};
+nonMigHaDx = {'non-migraine headache'};
 migraineIdx = or(strcmp(migraineDx{1},cellstr(Results.DxFull)),...
     strcmp(migraineDx{2},cellstr(Results.DxFull)));
+otherHaIdx = or(...
+    or(strcmp(probMigraine{1},cellstr(Results.DxFull)),...
+    strcmp(probMigraine{2},cellstr(Results.DxFull))),...
+    strcmp(nonMigHaDx,cellstr(Results.DxFull)) );
 haFIdx = strcmp(haFDx,cellstr(Results.DxFull));
-allIdx = or(migraineIdx,haFIdx);
+allIdx = 1:size(Results,1);
 
-%% US Map of Migraine respondants
+
+
+%% US Map of all respondants
 figHandle = figure();
 set(gcf, 'color', 'none');
 figuresize(4,3,'inches');
 latVec = double(T.LocationLatitude);
 lonVec = double(T.LocationLongitude);
-gp = geoplot(latVec(migraineIdx),lonVec(migraineIdx),'o','MarkerSize',5,'MarkerEdgeColor','none','MarkerFaceColor',[1 0 0]);
+gp = geoplot(latVec(allIdx),lonVec(allIdx),'o','MarkerSize',5,'MarkerEdgeColor','none','MarkerFaceColor',[1 0 0]);
 geobasemap darkwater
 geolimits([25 50],[-130 -60]);
 a = gca();
 a.AxisColor = 'none';
 a.Grid = 'off';
 a.Scalebar.Visible = 'off';
-plotFileName = fullfile(figSaveDir,'POEM_v3.1_MapMigraineRespondents.pdf');
+plotFileName = fullfile(figSaveDir,'POEM_v3.1_MapAllRespondents.pdf');
 saveas(figHandle,plotFileName);
 
 
-%% Conditional symptom scores, version 1
+%% Create symptom score plots for each group
+varsToPlot = {'MIDAS','AllodyniaScore','LightSensScore'};
+scoreLabel = {'MIDAS','allodynia','light sensitivity'};
+groups = {'haFIdx','otherHaIdx','migraineIdx'};
+groupLabels = {'HAF','otherHA','migr'};
+ylimMax = [0.75 0.75 0.75];
+xlimMax = [400,25,75];
+xtickSpacing = [100 5 25];
+plotPatchColors = {'k','y','r'};
+plotLineColors = {[0.5 0.5 0.5],[0.75 0.75 0.5],[0.75 0.5 0.5]};
+figHandle = figure();
+set(gcf, 'color', 'none');
+figuresize(length(varsToPlot)*2,2,'inches');
+tiledlayout(1,length(varsToPlot),'TileSpacing','tight','Padding','tight');
+for vv = 1:length(varsToPlot)
+
+    vals = double(Results.(varsToPlot{vv}));
+    [~,edges] = discretize(vals(allIdx),nBins);
+    X = edges(1:end-1) + diff(edges)/2;
+    nexttile(vv)
+    plot([0 xlimMax(vv)],[0 0],':k');
+
+    veridicalVals = [];
+    for gg = 1:length(groups)
+
+        idx = eval(groups{gg});
+        veridicalVals{gg} = vals(idx);
+        nSubs(gg) = sum(idx);
+
+        if vv == 1 && gg == 1
+           continue
+        end
+
+        Yboot = [];
+        for bb = 1:nBoots
+            bootIdx = datasample(find(idx),sum(idx));
+            Y = histcounts(vals(bootIdx),edges);
+            Yboot(bb,:) = Y ./ sum(Y);
+        end
+        Y = mean(Yboot,1);
+        Ysem = std(Yboot,[],1);
+        patch([X,fliplr(X)],[Y+Ysem,fliplr(Y-Ysem)],plotPatchColors{gg},'FaceAlpha',0.1,'EdgeColor','none');
+        hold on
+        lineHandles(gg) = plot(X,Y,'.-','Color',plotLineColors{gg},'MarkerSize',10,'LineWidth',1.5);
+    end
+
+    % Clean up
+    a = gca();
+    a.Color = 'none';
+    box off
+            a.YTick = 0:0.75:ylimMax(vv);
+    switch vv
+        case 1
+            ylabel('Proportion subjects');
+        case 2
+            a.YTick = [];
+        case 3
+            a.YTick = [];
+            for gg = 1:length(groups)
+                lgndText{gg} = sprintf([groupLabels{gg} ', n=%d'],nSubs(gg));
+            end
+            lh = legend(lineHandles,lgndText,'Location','northeast');
+            lh.Box = 'off';
+    end
+    ylim([-0.05,ylimMax(vv)]);
+    xlim([0,xlimMax(vv)]);
+    a.TickDir = 'out';
+    title(varsToPlot{vv});
+    xlabel(scoreLabel{vv});
+    a.XTick = 0:xtickSpacing(vv):xlimMax(vv);
+    a.XTickLabelRotation = 45;
+end
+plotFileName = fullfile(figSaveDir,'POEM_v3.1_GroupSymptomHistograms.pdf');
+saveas(figHandle,plotFileName);
+
+
+
+%% Conditional symptom scores\
 varsToPlot = {'MIDAS','AllodyniaScore','LightSensScore'};
 scoreLabel = {'MIDAS','allodynia','light sensitivity'};
 ylimMax = [0.75,0.75,0.75,];
@@ -87,6 +172,11 @@ for vv = 1:length(varsToPlot)
             veridicalVals{pp} = vals(idx);
 
             nSubs(pp) = sum(idx);
+
+            if nSubs(pp) == 0
+                continue
+            end
+
             Yboot = [];
             for bb = 1:nBoots
                 bootIdx = datasample(find(idx),sum(idx));
@@ -100,8 +190,14 @@ for vv = 1:length(varsToPlot)
             lineHandles(pp) = plot(X,Y,'.-','Color',plotLineColors{cc}{pp},'MarkerSize',10,'LineWidth',1.5);
         end
 
+
         % Perform a two-tailed KS test of the distributions
-        [~,p,ks2stat] = kstest2(veridicalVals{1},veridicalVals{2});
+        if all(nSubs > 0)
+            [~,p,ks2stat] = kstest2(veridicalVals{1},veridicalVals{2});
+        else
+            p = nan;
+            ks2stat = nan;
+        end
 
         % Clean up
         a = gca();
@@ -140,27 +236,11 @@ for vv = 1:length(varsToPlot)
     end
 
 end
-
-plotFileName = fullfile(figSaveDir,'POEM_v3.1_ConditionSymptomHistograms.pdf');
+plotFileName = fullfile(figSaveDir,'POEM_v3.1_ConditionalSymptomHistograms.pdf');
 saveas(figHandle,plotFileName);
 
 
-%% Contingent symptom count
-fprintf('Condition counts:\n');
-for ii = 1:length(conditionOn)
-    lineOut = [conditionOn{ii} ' -- '];
-    for jj = 1:ii-1
-        idx = and(...
-            contains(cellstr(Results.(conditionOn{ii})),conditionPairs{ii}{2}),...
-            contains(cellstr(Results.(conditionOn{jj})),conditionPairs{jj}{2})...
-            );
-        idx = and(migraineIdx,idx);
-        lineOut = sprintf([lineOut '%d  '],sum(idx));
-    end
-    fprintf([lineOut '\n']);
-end
-
-%% Correlation of measures
+%% Correlation of measures in people with migraine
 [rho,pval]=corr(Results.MIDAS(migraineIdx),Results.AllodyniaScore(migraineIdx),'Type','Spearman');
 fprintf('Correlation of MIDAS with ASC-12 in migraine: Spearman rho = %2.2f, p = %0.1e\n',rho,pval);
 
